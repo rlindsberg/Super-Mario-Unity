6b3196d97a100ecf6371274c61ea9db97c2dec2f
Roderick@Job 3:14 <roderick@karlemstrand.com>
2019-08-23 15:41:03 +0200

    coin destroy and add score

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int coinValue = 1;
+ 
+    private void OnCollisionEnter(Collision collision)
+    {
+        //name of component we look for
+        var coinScript = collision.gameObject.GetComponent<CoinCollector>();
+
+        if (coinScript != null)
+        {
+            coinScript.AddPoint(coinValue);
+            Destroy(gameObject);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private bool canJump = true;
+    
+    public Rigidbody MyRigidBody;
+    // default 5m/s
+    public float speed = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Exactly Horizontal/Jump... because of they are keys
+        float horizontalInput = Input.GetAxis("Horizontal");
+        
+        //move 1m/s
+        float movement = speed * horizontalInput * Time.deltaTime;
+        Debug.Log(horizontalInput);
+        
+        //shift in x-axis
+        //0 on other stuff
+        transform.position += new Vector3(movement, 0, 0);
+        
+        if (canJump && Input.GetButtonDown("Jump"))
+        {
+            MyRigidBody.AddForce(Vector3.up * 500);
+            canJump = false;
+        }
+
+
+
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        canJump = true;
+    }
+}
