7e9e34fa2ac1682cd65507adb45c0bba2a7cc103
Roderick@Job 3:14 <roderick@karlemstrand.com>
2019-08-23 13:55:41 +0200

    Initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody MyRigidBody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            MyRigidBody.AddForce(Vector3.up * 100);
+        }
+
+
+    }
+}
