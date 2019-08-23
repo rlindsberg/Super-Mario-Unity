5a77c941db6e4271acf6fa8e3eba743f295d642d
Roderick@Job 3:14 <roderick@karlemstrand.com>
2019-08-23 14:13:34 +0200

    change speed

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public Rigidbody MyRigidBody;
+    // default 5m/s
+    public float speed = 10;
     
     // Start is called before the first frame update
     void Start()
@@ -17,11 +19,14 @@ public class Movement : MonoBehaviour
     {
         // Exactly Horizontal/Jump... because of they are keys
         float horizontalInput = Input.GetAxis("Horizontal");
+        
+        //move 1m/s
+        float movement = speed * horizontalInput * Time.deltaTime;
         Debug.Log(horizontalInput);
         
         //shift in x-axis
         //0 on other stuff
-        transform.position += new Vector3(horizontalInput, 0, 0);
+        transform.position += new Vector3(movement, 0, 0);
         
         if (Input.GetButtonDown("Jump"))
         {
